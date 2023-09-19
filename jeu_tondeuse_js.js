be6663f30bd71ed664f1681471e07162ca332214
msnbrest <74934344+msnbrest@users.noreply.github.com>
2023-09-19 23:38:51 +0200

    svg grandma txt, func road to obj...

--- jeu_tondeuse_js.js
+++ jeu_tondeuse_js.js
@@ -1,53 +1,73 @@
-const ptet_couper= _=>{   if( game.me_stock<game.me_space ){   game.terrain[game.me_pos]= 0;   game.me_stock++;   }   },
+const init= _=>{
 
+game= {
+	system: {}, g_tile: {}, all: {
+		sizeN: 10, tiles: [], sizew: 75,
+		me_gas: 8, me_max_gas: 50, gas_bonus: 35, gas_pos: 28,
+		me_stock: null, me_space: 8, me_pos: 11,
+		composts: {
+			pos: [null,null],
+			stock: [0,12],
+			space: 12,
+		},
+	},
+	tiles_videy: null, eloigner: null, moved: null,
+	compostTouch: null,
+};
 
+game.system.over= type=>{
 
-draw_tile= (num,is_ton,is_gas,niv_com1)=> "<div class='tile tile_back"+ num +"'>"
-	+( is_gas?"<aside class='gas'></aside>":"" )
-	+( niv_com1<1?"":"<aside class='compost_"+( niv_com1-1<game.composts.space?"min":"max" )+"'></aside>" )
-	+( is_ton?"<aside class='ton'></aside><aside class='ton_gas'></aside>":"" )
-	+"</div>",
+	if( type==null ){   _sel(".over").style.display= "none";   return;   } // null==cacher
+	_sel(".over").style.display= "block";
+	_sel(".over").innerHTML= tab_html_over[ type ]();
+
+};
+
+game.system.retry= osef=>{
+
+	game.all.me_gas= 8;
+	game.all.me_pos= 11;
+	game.all.me_stock= 0;
+	game.all.gas_bonus= 35;
+	game.all.composts.pos[0]= game.all.sizeN-1;
+	game.all.composts.pos[1]= (game.all.sizeN-1)*game.all.sizeN;
+	game.all.composts.stock[0]= 0;
+	game.all.composts.stock[1]= 12;
+
+	game.all.tiles= [...Array(game.all.sizeN*game.all.sizeN)].map( vv=> 0 );
+	game.all.tiles[14]= 1;
+
+	game.system.over(null);
+	ku({keyCode:39});
+
+};
+
+game.g_tile.couper= _=>{
+
+	if( game.all.me_stock<game.all.me_space ){
+		game.all.tiles[game.all.me_pos]= 0;
+		game.all.me_stock++;
+
+		if( game.all.tiles.findIndex( vv=> vv==1 )<0 ){
+			// mamie contente + agrandi terrain
+			game.system.over("win");
+			game.all.me_stock= null;
+			game.tripledroite= 0;
+		}
+
+	}
+
+};
+
+game.g_tile.draw= (num,is_ton,is_gas,niv_com1)=> "<div class='tile tile_back"+ num +"'>"
++( is_gas?"<aside class='gas'></aside>":"" )
++( niv_com1<1?"":"<aside class='compost_"+( niv_com1-1<game.all.composts.space?"min":"max" )+"'></aside>" )
++( is_ton?"<aside class='ton'></aside><aside class='ton_gas'></aside>":"" )
++"</div>";
 // regle: toujours 'gas' en dernier
-// scotch: niv_com1 est chiant mais marche bien (pas de -1 ou null, minimum est 0 donc niveau minimum "+1" ...)
+// scotch: niv_com1... (pas de -1 ou null, minimum est 0 donc niveau minimum "+1" ...)
 
-
-
-init= _=>{   init_obj();   help.start(null,false);   setTimeout( init2, 2000 );   },
-
-
-
-init2= osef=>{   game.me_stock= 0;   game.terrain= [...Array(game.size*game.size)].map( vv=> 1 );   game.composts.pos[1]= game.size*game.size-1;   fullscreen.resetsize( false );   ku({keyCode:39});   },
-// scotch: game.me_stock null == game waiting
-
-
-
-resize= osef=>{   fullscreen.resetsize( false );   },
-
-
-
-init_obj= _=>{
-
-	game= { size: 10, terrain: [], me_gas: 8, me_max_gas: 50, gas_bonus: 35, me_stock: null, me_space: 8, me_pos: 11, gas_pos: 28, sizew: 75,
-		composts: { pos:[0,null], stock:[0,12], space:12 }
-	};
-	_sel(".over").style.display= "none";
-
-},
-
-
-
-redraw_all= all=>{
-
-	// todo: if need, ameliorer perf
-	_sel("#cssjs_tondeuse").innerHTML= "aside.ton_gas{width:"+( game.me_gas<10?game.me_gas*game.sizew/7.5:((game.me_gas-game.sizew/10)*game.sizew/37.5) )+"px;height:4px;background:#"+( game.me_gas<game.size?"f20":"2b2" )+";}"+fullscreen.css;
-	_sel("#stats").innerHTML= "Essence: "+game.me_gas+"/"+game.me_max_gas+"<br>Stock: "+game.me_stock+"/"+game.me_space+"<br>Compost1: "+game.composts.stock[0]+"/"+game.composts.space+"<br>Compost2: "+game.composts.stock[1]+"/"+game.composts.space;
-	all &&( _sel(".ingame").innerHTML= game.terrain.map( (vv,kk)=> draw_tile( vv, kk==game.me_pos, kk==game.gas_pos, (kk==game.composts.pos[0]?game.composts.stock[0]+1:0)+(kk==game.composts.pos[1]?game.composts.stock[1]+1:0) ) ).join("") );
-
-},
-
-
-
-loin_du_joueur= _=>{
+game.eloigner= _=>{
 
 	let choix= [];
 	let mult= [-6,-5,-4,-3,3,4,5,6];
@@ -56,74 +76,97 @@ loin_du_joueur= _=>{
 	for( let curxy=0, lig= mult.length, len=lig*lig; curxy<len; curxy++ ){
 		px= mult[curxy%lig];
 		py= mult[curxy/lig|0];
-		if( game.me_pos+py*game.size>-1 && game.me_pos+py*game.size<game.size*game.size && game.me_pos%game.size+px>-1 && game.me_pos%game.size+px<game.size ){
-			choix.push( game.me_pos+px+py*game.size );
+		if( game.all.me_pos+py*game.all.sizeN>-1 && game.all.me_pos+py*game.all.sizeN<game.all.sizeN*game.all.sizeN && game.all.me_pos%game.all.sizeN+px>-1 && game.all.me_pos%game.all.sizeN+px<game.all.sizeN ){
+			choix.push( game.all.me_pos+px+py*game.all.sizeN );
 		}
 	}
 
-	if( choix.length<1 ){
-		return 0;
-	}
+	if( choix.length<1 ){ return 0; }
 	return choix[Math.random()*choix.length|0];
 
-},
+};
 
-
-
-tap_compost= num=>{
+game.compostTouch= num=>{
 
 	// deplacer selon possible
-	const transfert= Math.min( game.composts.space-game.composts.stock[num], game.me_stock );
-	game.composts.stock[num]+= transfert;
-	game.me_stock-= transfert;
+	const transfert= Math.min( game.all.composts.space-game.all.composts.stock[num], game.all.me_stock );
+	game.all.composts.stock[num]+= transfert;
+	game.all.me_stock-= transfert;
 	// vider l'autre si lui est remplis
-	game.composts.stock[num]<game.composts.space ||( game.composts.stock[1-num]= 0 );
+	game.all.composts.stock[num]<game.all.composts.space ||( game.all.composts.stock[1-num]= 0 );
+
+};
+
+game.moved= event=>{
+
+	if( event.keyCode==38 && game.all.me_pos>game.all.sizeN-1 ){ game.all.me_pos-= game.all.sizeN; return 1; } // haut
+	if( event.keyCode==40 && game.all.me_pos<game.all.sizeN*(game.all.sizeN-1) ){ game.all.me_pos+= game.all.sizeN; return 1; } // bas
+	if( event.keyCode==37 && game.all.me_pos%game.all.sizeN>0 ){ game.all.me_pos-= 1; return 1; } // gauche
+	if( event.keyCode==39 && game.all.me_pos%game.all.sizeN<(game.all.sizeN-1) ){ game.all.me_pos+= 1; return 1; } // droite
+	return 0;
+
+};
+
+fullscreen.resetSize( false );
+help.start(null,false);
+setTimeout( game.system.retry, 1500 );
 
 },
 
 
 
-game_retry= _=>{   init_obj();   init2(null);   },
+tab_html_over= {
+
+"win": _=> `<div class='tile4x4 grandma ma'><div class='grandmamess win'></div></div><span onclick="game.system.retry(null);" class='tag btn big jaune'>Rejouer</span><br><br><span class='c666'>Appuyez 3 fois sur droite pour rejouer</span>`,
+
+"loose": _=> `<div class='tile4x4 grandma ma'><div class='grandmamess loose'></div></div><span onclick="game.system.retry(null);" class='tag btn big jaune'>Rejouer</span><br><br><span class='c666'>Appuyez 3 fois sur droite pour rejouer</span>`,
+
+},
+
+
+
+redraw= all=>{
+
+	// todo: if need, ameliorer perf
+	_sel("#cssjs_tondeuse").innerHTML= "aside.ton_gas{width:"+( game.all.me_gas<10?game.all.me_gas*game.all.sizew/7.5:((game.all.me_gas-game.all.sizew/10)*game.all.sizew/37.5) )+"px;height:4px;background:#"+( game.all.me_gas<game.all.sizeN?"f20":"2b2" )+";}"+fullscreen.css;
+
+	_sel("#stats").innerHTML= "Essence: "+game.all.me_gas+"/"+game.all.me_max_gas+"<br>Stock: "+game.all.me_stock+"/"+game.all.me_space+"<br>Compost1: "+game.all.composts.stock[0]+"/"+game.all.composts.space+"<br>Compost2: "+game.all.composts.stock[1]+"/"+game.all.composts.space;
+
+	all &&( _sel(".ingame").innerHTML= game.all.tiles.map( (vv,kk)=> game.g_tile.draw( vv, kk==game.all.me_pos, kk==game.all.gas_pos, (kk==game.all.composts.pos[0]?game.all.composts.stock[0]+1:0)+(kk==game.all.composts.pos[1]?game.all.composts.stock[1]+1:0) ) ).join("") );
+
+},
 
 
 
 ku= event=>{
 
-if( game.me_stock == null ){
+if( game.all.me_stock == null ){
 	// trois fois droite puis ptet retry
-	event.keyCode==39 ?( game.tripledroite++, game.tripledroite>2 &&( game_retry() ) ):( game.tripledroite= 0 );
-	return false;
-}else if( game.me_gas<1 ){
-	// perdu
-	_sel(".over").style.display= "block";
-	game.me_stock= null;
-	game.tripledroite= 0;
+	event.keyCode==39 ?( game.tripledroite++, game.tripledroite>2 &&( game.system.retry(null) ) ):( game.tripledroite= 0 );
 	return false;
 }
 
-if( _moved(event) ){
-	game.me_gas--;
+if( game.moved(event) ){
+	game.all.me_gas--;
 	// detruire terrain ou conso exageree
-	if( game.terrain[game.me_pos] == 1 ){   ptet_couper();   }else{   game.me_gas--;   }
+	if( game.all.tiles[game.all.me_pos] == 1 ){   game.g_tile.couper();   }else{   game.all.me_gas--;   }
+
+	if( game.all.me_gas<1 ){
+		// perdu
+		game.all.me_stock= null;
+		game.tripledroite= 0;
+		redraw(1);
+		setTimeout( "game.system.over('loose');", 750 );
+		return false;
+	}
 	// recharger
-	if( game.me_pos == game.gas_pos ){   game.gas_pos= loin_du_joueur();   game.me_gas= Math.min( game.me_max_gas, game.me_gas+game.gas_bonus );   game.gas_bonus>20 &&( game.gas_bonus-=2 );   }
+	if( game.all.me_pos == game.all.gas_pos ){   game.all.gas_pos= game.eloigner();   game.all.me_gas= Math.min( game.all.me_max_gas, game.all.me_gas+game.all.gas_bonus );   game.all.gas_bonus>20 &&( game.all.gas_bonus-=2 );   }
 	// vider stock
-	if( game.me_pos == game.composts.pos[0] ){ tap_compost(0); }
-	if( game.me_pos == game.composts.pos[1] ){ tap_compost(1); }
-	redraw_all(true);
+	if( game.all.me_pos == game.all.composts.pos[0] ){ game.compostTouch(0); }
+	if( game.all.me_pos == game.all.composts.pos[1] ){ game.compostTouch(1); }
+
+	redraw(1);
 }
-
-},
-
-
-
-_moved= event=>{
-
-	if( event.keyCode==38 && game.me_pos>game.size-1 ){ game.me_pos-= game.size; return true;; } // haut
-	if( event.keyCode==40 && game.me_pos<game.size*(game.size-1) ){ game.me_pos+= game.size; return true;; } // bas
-	if( event.keyCode==37 && game.me_pos%game.size>0 ){ game.me_pos-= 1; return true;; } // gauche
-	if( event.keyCode==39 && game.me_pos%game.size<(game.size-1) ){ game.me_pos+= 1; return true;; } // droite
-	return false;
 
 },
 
@@ -134,22 +177,25 @@ fullscreen= {
 is: 0,
 
 css: "",
+
 css_hidden: "header,footer{display:none;}body{margin:0;}main.my08.py20{padding:0;margin:0 auto;}main.mxw1200px{max-width:100%;width:100%;}",
 
-resetsize: sw=>{
+resetSize: sw=>{
 
     sw &&( fullscreen.is= !fullscreen.is ); // switch is need
     const kdo= document.documentElement;
     let o_masta= kdo.clientHeight < kdo.clientWidth ? kdo.clientHeight: kdo.clientWidth;
-    game.sizew= ((o_masta-(fullscreen.is?0:180))/game.size);
-    
-    fullscreen.css= ".plateau_w{width:"+ game.size*game.sizew +"px}.tile,aside{width:"
-        + game.sizew +"px;height:"+ game.sizew +"px;}aside.ton_gas{top:"
-        + game.sizew*0.95 +"px;}"
-        + (fullscreen.is?fullscreen.css_hidden:"");
-    redraw_all(false);
+    game.all.sizew= (o_masta-(fullscreen.is?0:180))/game.all.sizeN;
 
-}
+    fullscreen.css= ".plateau_w{width:"+ game.all.sizeN*game.all.sizew +"px}.tile,aside{width:"
+        + game.all.sizew +"px;height:"+ game.all.sizew +"px;}.tile4x4{width:"
+        + (game.all.sizew*4) +"px;height:"+ (game.all.sizew*4) +"px;}aside.ton_gas{top:"
+        + game.all.sizew*0.95 +"px;}"
+        + (fullscreen.is?fullscreen.css_hidden:"");
+
+    redraw(0);
+
+},
 
 },
 
@@ -178,21 +224,21 @@ tick: osef=>{
 
 txts:[
 	{txt:"Bienvenue !",time:1000},
-	{txt:"|",time:500},
+	{txt:"|",time:200},
 	{txt:"Pret à tondre ?",time:1500},
-	{txt:"|",time:250},
+	{txt:"|",time:200},
 	{txt:"Le but est de tondre entierement la pelouse.",time:2500},
-	{txt:"|",time:250},
+	{txt:"|",time:200},
 	{txt:"Déplacements avec les fleches directionelles",time:3000},
-	{txt:"|",time:250},
+	{txt:"|",time:200},
 	{txt:"Attention au niveau de carburant",time:2500},
-	{txt:"|",time:250},
+	{txt:"|",time:200},
 	{txt:"Les bidons remontent le niveau du réservoir.",time:3000},
-	{txt:"|",time:250},
+	{txt:"|",time:200},
 	{txt:"Rouler sur l'herbe coupée génère un Malus.",time:3000},
-	{txt:"|",time:250},
+	{txt:"|",time:200},
 	{txt:"Le bac de ramassage est à vider dans le compost,",time:3500},
-	{txt:"|",time:250},
+	{txt:"|",time:200},
 	{txt:"il faut remplir un compost pour débloquer l'autre.",time:3500},
 	{txt:"[aide]",time:50},
 ]
@@ -201,13 +247,16 @@ txts:[
 
 
 
-// todo idee : recréer de l'herbe lorsque full
-// collecter 25+ bidons sans terminer l'herbe
-// collecter 50 bidons apres avoir coupé l'herbe plateau vidé
-// toucher les 4 coins de l'écran apres avoir coupé l'herbe
-// pierre casse lame pendant 50chemin
-// relancer lorsque perdu
-// augmenter taille lorsque gagné, donc + bidons mais 1compost
+// todo idee :
+// Trophee, collecter 25+ bidons sans terminer l'herbe
+// Trophee, collecter 50 bidons apres avoir coupé l'herbe plateau vidé
+// Trophee, toucher les 4 coins de l'écran apres avoir coupé l'herbe
+// Trophee, chopper essence sur la case compost + non tondue xD
+// recréer de l'herbe random lorsque full
+// trois coeurs.   dechets, panne = -1, attente x temps = recup +1, pierre = -3, fleur = -1.
+// Augmenter taille lorsque gagné, donc + bidons mais memes compost
+// Power up : prout qui propulse de 3 cases, conso essence ( à sec ? marche quand meme )
+// perdu = mini jeu pour la réparer soit même (selon essence ou lame hs)
 
 
 
@@ -216,6 +265,6 @@ let game= {};
 
 
 
-window.addEventListener("load",init,false);
-window.addEventListener("keyup",ku,false);
-window.addEventListener("resize",resize,false);
+window.addEventListener("load", init, false);
+window.addEventListener("keyup", ku, false);
+window.addEventListener("resize", osef=>{ fullscreen.resetSize( false ); }, false);
