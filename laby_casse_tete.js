0109d46ae383dc203e26c2466658b1aac374ce81
msnbrest <74934344+msnbrest@users.noreply.github.com>
2023-12-10 19:34:18 +0100

    petite MAJ : max score, last score, time, menu

--- laby_casse_tete.js
+++ laby_casse_tete.js
@@ -1,16 +1,10 @@
 const init= _=>{
 
-	// init de base
-	new_score(0);
-	setTimeout("_sel('#temp_help').style.background='#ff04';",400);
-	setTimeout("_sel('#temp_help').style.background='#fff0';",600);
-	setTimeout("_sel('#temp_help').style.background='#ff04';",1400);
-	setTimeout("_sel('#temp_help').style.background='#fff0';",1600);
-	setTimeout("_sel('#temp_help').style.background='#ff04';",2400);
-	setTimeout("_sel('#temp_help').style.background='#fff0';",2600);
-	setTimeout("_sel('#temp_help').remove();",8500);
+	me= {};
+	// -- config --
+	me.len= 10;
 
-	// création tableau html
+	// -- création tableau html --
 	(grid=>{
 
 		if( !grid ){ return; }
@@ -23,9 +17,57 @@ const init= _=>{
 
 	})( document.querySelector("#grid") );
 
+	// -- init de base --
+
+	menu_game(0);
+	setTimeout("_sel('#menu_game').style.background='#ff04';",200);
+	setTimeout("_sel('#menu_game').style.background='#fff0';",300);
+	setTimeout("_sel('#menu_game').style.background='#ff04';",700);
+	setTimeout("_sel('#menu_game').style.background='#fff0';",800);
+	setTimeout("_sel('#menu_game').style.background='#ff04';",1200);
+	setTimeout("_sel('#menu_game').style.background='#fff0';",1300);
+	setInterval(loop,1000);
+
+},
+
+
+
+menu_game_phrases= [
+
+`But : monter &nbsp; | &nbsp; Touches : haut, bas, gauche, droite. <button onclick='menu_game(1)'>Jouer</button>`,
+`<button onclick='menu_game(0)'>Astuces</button> Mode de jeu : <button onclick='reset_game()'>Libre</button> | <button onclick='reset_game( 120, 100 )'>Limité</button> | <button onclick='reset_game( 120, 250 )'>Hard</button>`,
+],
+
+
+
+menu_game= val=>{
+
+	menu_game_phrases[val] &&( _sel("#menu_game").innerHTML= menu_game_phrases[val] );
+},
+
+
+
+reset_game= ( _maxduration=null, _maxheight=null, )=>{
+
+	// Info: x=pos, y=pos, decal_y=offset start, len=largeur, pics=picots limitants (zero et...), score, grille=draw & technique
+	me.x= 0;
+	me.y= 0;
+	me.decal_y= 3;
+	me.pics= [0,3];
+	me.grille= [];
+	me.maxduration= _maxduration;
+	me.duration= 0;
+	me.maxheight= _maxheight;
+	
+	new_now_score(0);
+	new_best_score(0);
+	new_last_score(0);
+	new_oups(0);
+
 	// init grille
 	add_line(1); // regle : toujours au moins une ligne pleine en bas, pour éviter soucis out of bound, negative index array et generation need last line
 	add_line(1);
+
 	add_line(1);
 	add_line(0);
 
@@ -34,33 +76,70 @@ const init= _=>{
 	// finalisation
 	me.y= me.decal_y; // prepa me
 	maybe_move(0,0); // redraw
-
 },
 
 
 
 maybe_move= (dirx,diry)=>{
 
-	const obj= maybe_coll(dirx,diry);
-	if( obj==null ){ return; }
+	const obj= test_move(dirx,diry);
+	if( obj==null ){
+		new_oups( me.oups+1 );
+		return;
+	}
 	me.x= obj.x;
 	me.y= obj.y;
-	me.score< me.y-me.decal_y && new_score(me.y-me.decal_y);
+	me.now_score< me.y-me.decal_y && new_now_score(me.y-me.decal_y);
 	maybe_add_random_line(me.len+me.y-me.decal_y);
 	redraw();
+	me.y-me.decal_y>=me.maxheight &&( game_win() );
 },
 
 
 
-new_score= val=>{
+new_now_score= val=>{
 
-	me.score= val;
-	_sel("#score").innerHTML= "Score : "+ me.score;
+	me.now_score= val;
+	if( me.best_score<me.now_score ){
+		new_best_score(me.now_score);
+	}
+	_sel("#now_score").innerHTML= `Score : ${me.now_score}` + (me.maxheight==null? "": ` / ${me.maxheight}`);
 },
 
 
 
-maybe_coll= (dirx,diry)=>{
+new_best_score= val=>{
+
+	me.best_score= val;
+	_sel("#best_score").innerHTML= "Best : "+ me.best_score;
+},
+
+
+
+new_last_score= val=>{
+
+	me.last_score= val;
+	_sel("#last_score").innerHTML= "Last : "+ me.last_score;
+},
+
+
+
+new_oups= val=>{
+
+	me.oups= val;
+	_sel("#oups").innerHTML= "Oups : "+ me.oups;
+},
+
+
+
+show_duration= val=>{
+
+	_sel("#duration").innerHTML= me.maxduration? `Time: ${me.duration} / ${me.maxduration}`: "";
+},
+
+
+
+test_move= (dirx,diry)=>{
 
 	let out={x:null,y:null};
 	// new pos
@@ -101,45 +180,28 @@ add_line= force=>{
 
 	// -- faire de la place intelligement, sur une ligne de jeu aléatoire --
 	const last_line= me.grille.slice(-1)[0];
-	// selon last line et ses positions possible, créer une nouvelle ligne fonctionelle
+	// -- selon last line et ses positions possible, créer une nouvelle ligne fonctionelle --
 
-
-console.info("--Create--");
-// poser x trou. (x étant un nombre totalement aléatoire)
+	//! poser x trou. (x étant un nombre totalement aléatoire)
 	ajouter_des_trous(new_line_obj.public,[]);
 
-console.info("--verif by private");
-console.info(JSON.stringify(last_line.private));
-// while pas some   private last_line curseur peut juste monter
+	//! while pas some   private last_line curseur peut juste monter
 	while( !last_line.private.some( (vv,kk)=> vv==1 && player_peut( new_line_obj.public, kk ) ) ){
-// -> ajouter au moins x trou. (x étant le nombre random by case restant /2 +1)
-console.info("+Retry");
-	ajouter_des_trous(new_line_obj.public,new_line_obj.public.map( (vv,kk)=> vv==0?kk:-1 ).filter( vv=> vv>-1 ));
-//alert("retry");
+		//! -> ajouter au moins x trou. (x étant le nombre random by case restant /2 +1)
+		ajouter_des_trous(new_line_obj.public,new_line_obj.public.map( (vv,kk)=> vv==0?kk:-1 ).filter( vv=> vv>-1 ));
 	}
-console.info("--OK--");
-// TODO lorsqu'on fera le private final, evidement que ce sera selon ligne precedente mais aussi selon les position validées de cette ligne alors on bougera le curseur pour tester depuis les pos de base :D, opti
 
-console.info("--priv");
-console.info(JSON.stringify(new_line_obj.private));
 	last_line.private.forEach( (vv,kk)=>{
 		// selon chaque possible de last_line, verif juste au dessus c'est possible.
 		if( vv==1 && player_peut( new_line_obj.public, kk ) ){
 			new_line_obj.private[kk]= 1;
 		}
 	});
-console.info(JSON.stringify(new_line_obj.private));
 
 	// selon possible de la nouvelle ligne, bouger un peu à gauche puis un peu à droite voir les autres possibles
 	let priv_atester= new_line_obj.private.map( (vv,kk)=> vv==1?kk:-1 ).filter( vv=> vv>-1 );
 	let priv_dejatestee= [];
-console.info("XX oh putain XX");
 	while( priv_atester.length>0 ){ // selon chaque possible
-//alert("go un tour");
-console.info("go un tour");
-console.info(JSON.stringify(priv_atester));
-console.info(JSON.stringify(priv_dejatestee));
-console.info(JSON.stringify(new_line_obj.private));
 		if( priv_dejatestee.includes( priv_atester[0] ) ){ // deja testee
 			priv_atester.splice(0,1);
 			continue;
@@ -158,7 +220,6 @@ console.info(JSON.stringify(new_line_obj.private));
 		priv_dejatestee.push(priv_atester[0]);
 		priv_atester.splice(0,1);
 	}
-console.info("fin");
 
 	me.grille.push( new_line_obj );
 	return;
@@ -170,12 +231,11 @@ ajouter_des_trous= (ptr_line_public,trous_deja)=>{
 
 	// nombre de trous à faire, secu 1 + rand max ou rand "restant/2+1"
 	const rand_1_9= 1+Math.random()*( trous_deja.length<1? me.len-1: ((me.len-trous_deja.length-1)/2) )|0;
-console.info(rand_1_9);
-console.info(JSON.stringify(ptr_line_public));
+
 	let tab= [...Array(me.len)].map( (vv,kk)=> kk ).filter( vv=> !trous_deja.includes(vv) ); // liste dont les trous sont ignorés
 	let out= 0; // liste de nouveaux trous
+
 	while(out<rand_1_9){ out++; const ind= tab.splice( Math.random()*tab.length|0 ,1 )[0]; ptr_line_public[ind]= 0; } // ajouter les trous à la liste
-console.info(JSON.stringify(ptr_line_public));
 },
 
 
@@ -203,6 +263,31 @@ sw_h= html_elem=>{
 
 
 
+game_loose= _=>{
+
+	alert("Perdu, appuyez sur OK pour rejouer");
+	new_last_score( me.now_score );
+	reset_game( me.maxduration, me.maxheight );
+},
+
+
+
+game_win= _=>{
+
+	alert("Gagné, appuyez sur OK pour rejouer");
+	new_last_score( me.now_score );
+	reset_game( me.maxduration, me.maxheight );
+},
+
+
+
+loop= osef=>{
+
+	me.maxduration==null ||( me.duration++, show_duration(), me.duration>me.maxduration&&( game_loose() ) );
+},
+
+
+
 kk= event=>{
 
 	if( event.keyCode==37 ){ maybe_move(-1, 0); } // gauche
@@ -213,10 +298,9 @@ kk= event=>{
 
 
 
-// x=pos, y=pos, decal_y=offset start, len=largeur, pics=picots limitants (zero et...), score, grille=draw & technique
-let me={ x:0, y:0, decal_y:3, len:10, pics:[0,3], score:0, grille:[] };
+let me= null;
 
-
+// TODO apres, faire "defiler" les cases, car là c'est un peu trop brut
 
 window.addEventListener("load",init,false);
 window.addEventListener("keydown",kk,0);
